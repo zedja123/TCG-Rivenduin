00bd17570eb19de28c144b4c41613760db371af2
zedja123 <miguel.zedja@hotmail.com>
2024-10-09 19:17:50 -0300

    Update Game.cs

--- TCG-Rivenduin/Assets/TcgEngine/Scripts/GameLogic/Game.cs
+++ TCG-Rivenduin/Assets/TcgEngine/Scripts/GameLogic/Game.cs
@@ -9,10 +9,13 @@ namespace TcgEngine
     [System.Serializable]
     public class Game
     {
+<<<<<<< HEAD
         public ResponsePhase response_phase = ResponsePhase.None;
         public float response_timer = 0f;
         public bool selector_cancelable;
 
+=======
+>>>>>>> parent of 13d5a49 (commit)
         public string game_uid;
         public GameSettings settings;
 
@@ -108,8 +111,6 @@ namespace TcgEngine
             Player player = GetPlayer(card.player_id);
             if (!skip_cost && !player.CanPayMana(card))
                 return false; //Cant pay mana
-            if (!skip_cost && response_phase == ResponsePhase.Response && !card.HasTrait("response"))
-                return false; //Cant response with it
             if (!player.HasCard(player.cards_hand, card))
                 return false; // Card not in hand
             if (player.is_ai && card.CardData.IsDynamicManaCost() && player.mana == 0)
@@ -150,8 +151,12 @@ namespace TcgEngine
         {
             if (card == null || !slot.IsValid())
                 return false;
+<<<<<<< HEAD
             if (response_phase == ResponsePhase.Response)
                 return false;
+=======
+
+>>>>>>> parent of 13d5a49 (commit)
             if (!IsOnBoard(card))
                 return false; //Only cards in play can move
 
@@ -179,8 +184,12 @@ namespace TcgEngine
 
             if (!attacker.CanAttack(skip_cost))
                 return false; //Card cant attack
+<<<<<<< HEAD
             if (response_phase == ResponsePhase.Response)
                 return false;
+=======
+
+>>>>>>> parent of 13d5a49 (commit)
             if (attacker.player_id == target.player_id)
                 return false; //Cant attack same player
 
@@ -548,7 +557,6 @@ namespace TcgEngine
             dest.turn_timer = source.turn_timer;
             dest.state = source.state;
             dest.phase = source.phase;
-            dest.response_phase = source.response_phase;
 
             if (dest.players == null)
             {
@@ -611,6 +619,7 @@ namespace TcgEngine
         SelectorChoice = 30,
         SelectorCost = 40,
     }
+<<<<<<< HEAD
     [System.Serializable]
     public enum ResponsePhase
     {
@@ -618,4 +627,6 @@ namespace TcgEngine
         Response = 10,
         ResponseSelector = 20,
     }
+=======
+>>>>>>> parent of 13d5a49 (commit)
 }
